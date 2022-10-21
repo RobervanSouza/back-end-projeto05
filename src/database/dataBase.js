f601084c9a3caee56d5805d486517dff047881c0
Robervan Souza <robervansouza91@gmail.com>
2022-10-21 14:27:40 -0300

    mongo atlas ok

--- src/database/dataBase.js
+++ src/database/dataBase.js
@@ -2,10 +2,17 @@ const mongoose = require('mongoose');
 
 const connectToDatabase = () => {
   mongoose
-  .connect('mongodb://localhost:27017/jogos-db', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }).then(()=> console.log("Mongoose Conectado")).catch((erro) => console.log(`Error ao conectar como mongoose Erro: ${erro}`),)
+    .connect(
+      'mongodb+srv://alice:admin@jogoxbox.98uba48.mongodb.net/?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    )
+    .then(() => console.log('Mongoose Conectado'))
+    .catch((erro) =>
+      console.log(`Error ao conectar como mongoose Erro: ${erro}`),
+    );
 }
 
 module.exports = connectToDatabase;
