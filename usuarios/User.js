dfcd2b154fd39e5963944141d8a6ded7c80e16db
Robervan Souza <robervansouza91@gmail.com>
2022-10-21 22:02:00 -0300

    criptografar senha

--- usuarios/User.js
+++ usuarios/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcryptjs');
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -33,6 +33,13 @@ const UserSchema = new mongoose.Schema({
     unique: true,
   },
 });
+
+UserSchema.pre('save', async function (next) {
+  this.password = await bcrypt.hash(this.password, 10)
+  next();
+});
+
+
 
 const User = mongoose.model('usuarios', UserSchema);
 
